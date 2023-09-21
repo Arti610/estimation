37311acadb03f2e473976aaae97e7eb167a580bd
Arti610 <artinirmalkar06@gmail.comL>
2023-09-21 10:59:03 +0530

    login UI

--- src/Common_Components/Login/Login.jsx
+++ src/Common_Components/Login/Login.jsx
@@ -4,21 +4,24 @@ import { TextField, Grid, InputAdornment, IconButton } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getTaxAgencyData } from "../../APIs/TaxAgencySlice";
 import { useCookies } from "react-cookie";
+import "./Login.css"
 import "../../Components/Modal/Modal.css"
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { userLogin } from "../../APIs/LoginSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Login = (props) => {
   const style = {
     position: "absolute",
-    top: "30%",
+    top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
     backgroundColor: "#fff",
-    border: "1px solid #6e85b7",
-    width: "700px",
+    // border: "1px solid #6e85b7",
+    width: "25%",
+    flexDirection: "column",
     borderRadius: "10px",
-    height: "fit-content",
+    display: "flex",
+    height: "55%",
     overflow: "auto",
     "@media (max-width: 576px)": {
       width: "90%",
@@ -40,7 +43,7 @@ const Login = (props) => {
     let userCredential = { email, password };
 
     dispatch(userLogin(userCredential)).then((result) => {
-       if (result) {
+      if (result) {
         setEmail('');
         setPassword('');
         navigate("/dashboard");
@@ -52,16 +55,16 @@ const Login = (props) => {
 
   return (
     <>
-
+  <div></div>
       <Box sx={style} className="scroll-bar">
-        <div className="modal-top-container">
-          <h4>Login</h4>
+        <div className="login_top_contianer">
+          <h2>LogIn</h2>
+          <p>Welcome back! Please enter your details</p>
         </div>
         <form onSubmit={handleSubmit}>
-
-
           <Grid item xs={12} sm={6} md={4}>
-            <label>
+           <div style={{margin:"20px 0px"}}>
+           <label>
               EMAIL <span style={{ color: "red" }}>*</span>
             </label>
             <TextField
@@ -73,12 +76,12 @@ const Login = (props) => {
               placeholder="example@gmail.com"
               fullWidth
               required
-
-
             />
+           </div>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <label>
+          <div>
+          <label>
               PASSWORD <span style={{ color: "red" }}>*</span>
             </label>
             <TextField
@@ -101,27 +104,37 @@ const Login = (props) => {
                 ),
               }}
             />
+          </div>
           </Grid>
 
 
-          <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <div style={{ width: "100%", display: "flex",height: "100px",display: "flex",alignItems: "flex-end", justifyContent: "center" }}>
             <button
               variant="outlined"
               type="submit"
               style={{
-                margin: "15px",
+                margin: "15px 20px",
+                width:"100%",
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"center",
                 fontSize: "12px",
+                padding:"10px 80px",
+                fontSize:"17px"
               }}
-              onClick={props.createOrUpdateHandler}
+            onClick={props.createOrUpdateHandler}
             >
-              {/* {loading ? "loading....." : "Login"} */}
-              login
-            </button>
-          </div>
-          {/* {error && <div className="error-message">{error}</div>} */}
+            {/* {loading ? "loading....." : "Login"} */}
+            LOGIN
+          </button>
+        </div>
+        <div className="login-powered">
+          <p>Powered by <Link to="https://nableinvent.com/" target="_blank">Nable</Link></p>
+        </div>
+        {/* {error && <div className="error-message">{error}</div>} */}
 
-        </form>
-      </Box>
+      </form>
+    </Box >
 
     </>
   );
