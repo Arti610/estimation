35b8786a1177821cf7c2d234fbd5adaa0b202fe0
Arti610 <artinirmalkar06@gmail.comL>
2023-11-24 10:22:09 +0530

    commiting live

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,9 +1,10 @@
 // Local Server
 // export const baseURL = "http://192.168.1.145:8000/api";
-export const baseURL = "http://127.0.0.1:8000/api";
+// export const baseURL = "http://127.0.0.1:8000/api";
+export const baseURL = "https://estimation-36jn.onrender.com/api";
 
 // export const ImgUrl = "http://192.168.1.145:8000";
-export const ImgUrl = "http://127.0.0.1:8000";
+export const ImgUrl = "https://estimation-36jn.onrender.com";
 
 // export const companyId = "2";
 // // //  Live Server
