ac2a2914235d96b591f1816e2082d767a8086d7b
Arti610 <artinirmalkar06@gmail.comL>
2023-12-04 14:53:42 +0530

    u

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -5,6 +5,6 @@
 // export const baseURL = "http://192.168.1.145:8000/api";
 // export const ImgUrl = "http://192.168.1.145:8000";
 
-export const baseURL = "https://estimation-36jn.onrender.com";
+export const baseURL = "https://estimation-36jn.onrender.com/api";
 export const ImgUrl = "https://estimation-36jn.onrender.com/media";
 
